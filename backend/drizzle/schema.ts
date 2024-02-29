e859d511e2c18eeb77f5e1380712867389ca891e
Matthieu Moreau <dev@matthieumoreau.fr>
2024-02-29 09:52:16 -0500

    [backend] Fix primary key in the schema

--- backend/drizzle/schema.ts
+++ backend/drizzle/schema.ts
@@ -1,7 +1,7 @@
-import { blob, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { blob, sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const images = sqliteTable("images", {
-  id: text("id"),
+  id: text('id').primaryKey(),
   file: blob("blob", { mode: "buffer" }),
   name: text("name").notNull().default(""),
   isReady: text("ready"),
